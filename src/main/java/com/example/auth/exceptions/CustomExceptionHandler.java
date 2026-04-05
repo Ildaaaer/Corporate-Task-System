51e1b6e316e317ddf02af2ff13f28affdb911dc0
Danil <danil.ildeickin@yandex.ru>
2026-04-06 02:46:46 +0400

    Добавлена база по исключениям

---
+++ src/main/java/com/example/auth/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.example.auth.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.BadRequestException;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Log4j2
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorDTO> handleBadRequest(BadRequestException ex) {
+        log.error("Bad request", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorDTO.builder()
+                        .error("Bad Request")
+                        .errorDescription(ex.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleAllExceptions(Exception ex) {
+        log.error("Unexpected error", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorDTO.builder()
+                        .error("Internal Server Error")
+                        .errorDescription(ex.getMessage())
+                        .build());
+    }
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<ErrorDTO> NotFoundException(Exception ex) {
+        log.error("Not found", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ErrorDTO.builder()
+                        .error("Not found")
+                        .errorDescription(ex.getMessage())
+                        .build());
+    }
+
+}
